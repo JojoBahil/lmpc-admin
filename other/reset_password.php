9f8967ef39c49fccdb836398e4a95c74080220a9
Jojo <jbahil47@gmail.com>
2022-04-25 16:09:13 +0800

    Modify the Email Content to be sent on reseting password

--- other/reset_password.php
+++ other/reset_password.php
@@ -13,7 +13,7 @@
     $student_portal = 'tes.lbp.app';
     $awardno = $_GET['awardno'];
 
-    $sqlDefaultPassword = "SELECT a.award_no, a.title, a.fname, a.mname, a.lname, a.def_password, a.email, b.hei_name, b.hei_uii, b.hei_region_nir FROM tbl_lbp_form a INNER JOIN tbl_heis b ON a.hei_uii = b.hei_uii WHERE award_no = '".$awardno."'";
+    $sqlDefaultPassword = "SELECT a.award_no, a.title, a.fname, a.mname, a.lname, a.def_password, a.email, a.status, b.hei_name, b.hei_uii, b.hei_region_nir FROM tbl_lbp_form a INNER JOIN tbl_heis b ON a.hei_uii = b.hei_uii WHERE award_no = '".$awardno."'";
     $resDefaultPassword = mysqli_query($connect, $sqlDefaultPassword);
     $checkDefaultPassword = mysqli_num_rows($resDefaultPassword);
     if($checkDefaultPassword > 0){
@@ -24,6 +24,7 @@
         $mname = $row['mname'];
         $lname = $row['lname'];
         $email = $row['email'];
+        $status = $row['status'];
         $hei_name = $row['hei_name'];
         $hei_uii = $row['hei_uii'];
         $hei_region_nir = $row['hei_region_nir'];
@@ -34,7 +35,7 @@
         if(mysqli_query($connect, $updateResetPassword)){
             // SEND EMAIL TO NOTIFY GRANTEE        
             $mailheader = 'FROM: UniFAST ICT Unit<unifastgov@chedvs01.ched.gov.ph>'."\r\n".'CC: unifast_teslmpc@ched.gov.ph'."\r\n".'Reply-To: unifast_teslmpc@ched.gov.ph'."\r\n".'MIME-Version: 1.0'."\r\n".'Content-type: text/html; charset=iso-8859-1';
-            $msg = "<b>Full Name: </b>".$fname." ".$mname." ".$lname."<br><b>TES Award Number: </b>".$award_no."<br><b>Region: </b>".$hei_region_nir."<br><b>HEI Unique ID: </b>".$hei_uii."<br><b>HEI Name: </b>".$hei_name."<br><b>Application Status: </b>Not Finalized<br><br><br><br>Good day ".$title.". ".$fname." ".$lname.",<br><p>Your application was unfinalized due to wrong input/image. Please make sure to <b>supply correct input/image</b> to the fields then click <b style='color:blue;'>Save</b> and <b style='color:green;'>Finalize</b> again.</p> Thank you.<br><br><br><br><b>Information and Communication Technology Unit</b><br>UniFAST Secretariat - Central Office";
+            $msg = "<b>Full Name: </b>".$fname." ".$mname." ".$lname."<br><b>TES Award Number: </b>".$award_no."<br><b>Region: </b>".$hei_region_nir."<br><b>HEI Unique ID: </b>".$hei_uii."<br><b>HEI Name: </b>".$hei_name."<br><b>Application Status: </b>".$status."<br><br><br><br>Good day ".$title.". ".$fname." ".$lname.",<br><p>Your password was reset to default due as requested. Please use the following credential to access your LMPC Portal account: <br><br> <b>Username: </b> ".$award_no." <br><b>Password: </b>".$def_password."</p><br> Thank you.<br><br><br><br><b>Information and Communication Technology Unit</b><br>UniFAST Secretariat - Central Office";
 
             $msg = wordwrap($msg,70);
 
